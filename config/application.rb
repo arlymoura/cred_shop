62ce01d0e5082dbdd943944364d07777562ac999
Arly Moura <arlymoura@gmail.com>
2024-11-28 20:52:13 -0300

    Add Brazilian Portuguese localization files and set default locale

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module InssDiscount
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_job.queue_adapter = :sidekiq
+    config.i18n.default_locale = :"pt-BR"
   end
 end
